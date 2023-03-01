d60c69b4b7dde4ffa6f17cd298231c451e829933
mattar13 <mtarchick13@gmail.com>
2023-03-01 14:49:37 -0500

    added some packages

--- src/ElectroPhysiology.jl
+++ src/ElectroPhysiology.jl
@@ -1,5 +1,18 @@
 module ElectroPhysiology
 
 # Write your package code here.
+## 1) Basic usage starts with the experiment Readers
+#=Import all experiment objects=======================#
+using Dates
+import Base: size, axes, length, getindex, setindex, sum, copy, maximum, minimum, push!, cumsum, argmin, argmax
+import Statistics.std
+
+include("Experiment/StimulusProtocol.jl")
+include("Experiment/Experiments.jl") #This file contains the Experiment structure. 
+
+#=Import all readers======================#
+include("Readers/ABFReader/ABFReader.jl")
+export readABF
+export parseABF
 
 end
