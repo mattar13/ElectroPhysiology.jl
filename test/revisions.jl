65f4bd90dc1f31d85eb616b86b83784fc669974e
mattar13 <mtarchick13@gmail.com>
2023-03-01 14:49:25 -0500

    created tests

---
+++ test/revisions.jl
@@ -0,0 +1,4 @@
+using Revise, ElectroPhysiology
+
+testfile = raw"test\to_filter.abf"
+data = readABF(testfile)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using ElectroPhysiology
 using Test
 
-@testset "ElectroPhysiology.jl" begin
-    # Write your tests here.
+@testset "Testing Experiement struct" begin
+    time = collect(1:0.01:1000) #Create some time range
+    data = rand(5, length(time), 1) #Create some dummy data
+
+    exp = ElectroPhysiology.Experiment(data)
+    @test !isnothing(exp)
+
+    exp_w_time = ElectroPhysiology.Experiment(time, data)
+    @test !isnothing(exp_w_time)
+end
+
+@testset "Testing ABF reader" begin
+    testfile = raw"to_filter.abf"
+    data = readABF(testfile)
+    @test !isnothing(data)
+
+
 end
