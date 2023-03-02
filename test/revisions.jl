3014940e0b4dbc7e7c9517d93c209261dcb80b67
mattar13 <mtarchick13@gmail.com>
2023-03-01 23:33:29 -0500

    added two sections to revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,5 +1,22 @@
 #None at this time
 using Revise, ElectroPhysiology
 
-test_file = raw"test/to_analyze.abf"
-data = readABF(test_file)
+#%% Section 1, Opening Matlab IRIS files
+PhysiologyAnalysis.__init__()
+using DataFrames, Query, XLSX
+using MAT
+file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\MAT files\2022-Feb-26_RBC_SPR.mat"
+data = matopen(file)
+vars = matread(file)
+
+#%% Section 2, Saving ABF files
+using Revise
+using PhysiologyAnalysis
+import PhysiologyAnalysis.saveABF
+import PhysiologyAnalysis.Experiment
+using PyPlot
+Revise.track(PhysiologyAnalysis, "src/Readers/ABFReader/ABFReader.jl")
+file_open = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Data\ERG\Paul\Cones\2019_07_23_WT_P14_m1\Cones\Drugs\Green\nd0.5_1p_1ms\19723190.abf"
+file_save = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Data\ERG\Paul\Cones\2019_07_23_WT_P14_m1\Cones\Drugs\Green\nd0.5_1p_1ms\test.abf"
+data = readABF(file_open, channels=-1) #This is necessary for saving
+saveABF(data, file_save)
