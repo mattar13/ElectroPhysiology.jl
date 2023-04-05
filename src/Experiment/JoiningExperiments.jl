8d94c4731b813e2a8e54ea3cbd797cbcbc511e85
mattar13 <mtarchick13@gmail.com>
2023-04-05 16:20:49 -0400

    updated documentation

--- src/Experiment/JoiningExperiments.jl
+++ src/Experiment/JoiningExperiments.jl
@@ -1,4 +1,29 @@
 """
+    concat(data::Experiment{T}, data_add::Experiment{T}; kwargs...) where {T}
+    concat!(data::Experiment{T}, data_add::Experiment{T}; kwargs...) where {T}
+
+Return a new `Experiment` object with `data` concatenated with `data_add` along the sweeps dimension.
+
+# Arguments
+- `data`: An `Experiment` object containing the experimental data.
+- `data_add`: Another `Experiment` object to be concatenated with the `data`.
+
+# Keyword Arguments
+- `mode`: Specifies whether to pad or chop the data if the sizes do not match (default: `:pad`).
+- `position`: Specifies the position to pad or chop (default: `:post`).
+- `verbose`: Prints information if set to true (default: false).
+- `channel_mode`: Specifies how to handle extra channels (default: `:remove_extra`).
+
+# Example
+```julia
+exp1 = Experiment(data_array1)
+exp2 = Experiment(data_array2)
+concatenated_exp = concat(exp1, exp2)
+```julia
+exp1 = Experiment(data_array1)
+exp2 = Experiment(data_array2)
+concat!(exp1, exp2)
+```
 """
 function concat(data::Experiment{T}, data_add::Experiment{T}; mode::Symbol=:pad, position::Symbol=:post, kwargs...) where {T}
     new_data = deepcopy(data)
@@ -54,6 +79,26 @@ function concat!(data::Experiment{T}, data_add::Experiment{T};
 end
 
 """
+    match_channels(exp1::Experiment, exp2::Experiment)
+
+Return two modified `Experiment` objects with their channels matched.
+
+If the number of channels in `exp1` and `exp2` is not equal, this function will drop extra channels
+from the `Experiment` object with more channels, making both objects have the same number of channels.
+
+# Arguments
+- `exp1`: An `Experiment` object containing the experimental data.
+- `exp2`: Another `Experiment` object to be matched with `exp1`.
+
+# Returns
+- `(exp1_modified, exp2_modified)`: A tuple containing the two modified `Experiment` objects with matched channels.
+
+# Example
+```julia
+exp1 = Experiment(data_array1)
+exp2 = Experiment(data_array2)
+exp1_modified, exp2_modified = match_channels(exp1, exp2)
+```
 """
 function match_channels(exp1::Experiment, exp2::Experiment)
     if size(exp1) != size(exp2)
@@ -70,7 +115,26 @@ function match_channels(exp1::Experiment, exp2::Experiment)
 end
 
 """
-This is just a convinent way to write subtraction as a function
+    sub_exp(exp1::Experiment, exp2::Experiment)
+
+Subtract the data of `exp2` from `exp1` and return a new `Experiment` object with the resulting data.
+
+This function will automatically match the channels of both `Experiment` objects if they do not match. 
+If the channels do not match, it will drop the unmatching channels by default.
+
+# Arguments
+- `exp1`: An `Experiment` object containing the experimental data.
+- `exp2`: Another `Experiment` object containing the data to be subtracted from `exp1`.
+
+# Returns
+- `data`: A new `Experiment` object with the resulting data after the subtraction.
+
+# Example
+```julia
+exp1 = Experiment(data_array1)
+exp2 = Experiment(data_array2)
+result_exp = sub_exp(exp1, exp2)
+```
 """
 function sub_exp(exp1::Experiment, exp2::Experiment)
     if size(exp1) == size(exp2)
