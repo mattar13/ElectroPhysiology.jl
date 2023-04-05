7299df7a967485769cf21dd3b3e40c84ab14c012
mattar13 <mtarchick13@gmail.com>
2023-04-05 16:44:50 -0400

    added a ton of auxillary functions

--- src/Readers/ABFReader/ABFReader.jl
+++ src/Readers/ABFReader/ABFReader.jl
@@ -1,42 +1,108 @@
+function extract_channel_indices(channels, HeaderDict)
+    if isa(channels, Vector{String})
+         return findall(ch -> ch ∈ channels, HeaderDict["adcNames"])
+    elseif isa(channels, Vector{Int64})
+         return channels
+    elseif channels == -1
+         return HeaderDict["channelList"]
+    else
+         return channels
+    end
+end
+
+function extract_channel_info(ch_idxs, HeaderDict)
+    ch_names = Vector{String}(HeaderDict["adcNames"][ch_idxs])
+    ch_units = Vector{String}(HeaderDict["adcUnits"][ch_idxs])
+    ch_telegraph = Vector{Float64}(HeaderDict["fTelegraphAdditGain"][ch_idxs])
+    return ch_names, ch_units, ch_telegraph
+end
+
+function extract_data(sweeps, channels, ch_names, HeaderDict, warn_bad_channel)
+    if sweeps == -1 && channels == -1
+         return HeaderDict["data"]
+    elseif sweeps == -1 && channels != -1
+         return getWaveform(HeaderDict, ch_names; warn_bad_channel=warn_bad_channel)
+    elseif sweeps != -1 && channels == -1
+         return HeaderDict["data"][sweeps, :, :]
+    elseif sweeps != -1 && channels != -1
+    data = getWaveform(HeaderDict, ch_names; warn_bad_channel=warn_bad_channel)
+         return data[sweeps, :, :]
+    end
+end
+
+function flatten_data(data)
+    n_size = size(data)
+    reshape_data = permutedims(data, (3, 2, 1))
+    reshape_data = reshape(reshape_data, 1, n_size[3], :)
+    return permutedims(reshape_data, (1, 3, 2))
+end
+
+function prepare_time_info(data, HeaderDict, time_unit)
+    dt = HeaderDict["dataSecPerPoint"]
+    idxs = collect(0:size(data,2)-1)
+    t = idxs .* dt
+    if time_unit == :ms
+         dt = 1000
+         t .= 1000
+    end
+    return dt, t
+end
+
+function extract_stimulus_protocol(HeaderDict, stimulus_name, stimulus_threshold)
+    if isnothing(stimulus_name)
+         return StimulusProtocol("Nothing")
+    else
+         return extractStimulus(HeaderDict; stimulus_name = stimulus_name, stimulus_threshold = stimulus_threshold)
+    end
+end
+
+function average_data_and_protocol(data, stimulus_protocol)
+    data = sum(data, dims=1) / size(data, 1)
+    stimulus_protocol_first_vals = stimulus_protocol[1]
+    stimulus_protocol = StimulusProtocol(stimulus_name)
+    stimulus_protocol[1] = stimulus_protocol_first_vals
+    return data, stimulus_protocol
+end     
+
 include("ByteMaps.jl") #These functions deal with the bytemap extractions
 include("Epochs.jl") #These functions deal with the Epochs
 include("WaveformExtraction.jl") #This imports the bytemaps for extracting the waveforms
 include("ReadHeaders.jl")
 
 #println("ABF utilites imported")
-
 """
-==================================================================
-Reading Function
+    readABF(::Type{T}, abf_data::Union{String,Vector{UInt8}};
+        sweeps::Union{Int64,Vector{Int64}}=-1,
+        channels::Union{Int64, Vector{String}}=["Vm_prime", "Vm_prime4"],
+        average_sweeps::Bool=false,
+        stimulus_name::Union{String, Vector{String}, Nothing}="IN 7",
+        stimulus_threshold::T=2.5,
+        warn_bad_channel=false,
+        flatten_episodic::Bool=false,
+        time_unit=:s,
+    ) where {T<:Real}
 
-This is the baseline function for reading ABF files. 
-==================================================================
-    experiment = readABF(type, filename; KWARGS)
-    experiment = readABF(filename; KWARGS)
+Read an Axon Binary File (ABF) and return an `Experiment` object. The function extracts data
+for the specified sweeps and channels, and optionally averages sweeps or flattens episodic data.
 
-ARGS:
-type::Type = The type in which all data will be converted to. Defaults to Float64. 
-filename::String = The filename that will be read
+# Arguments
+- `abf_data`: A `String` representing the ABF file path or a `Vector{UInt8}` containing the ABF file content.
+- `sweeps`: An `Int64` or a `Vector{Int64}` specifying the sweeps to extract. Default is -1 (all sweeps).
+- `channels`: An `Int64` or a `Vector{String}` specifying the channels to extract. Default is ["Vm_prime", "Vm_prime4"].
+- `average_sweeps`: A `Bool` specifying whether to average the sweeps. Default is `false`.
+- `stimulus_name`: A `String`, `Vector{String}`, or `Nothing` specifying the stimulus name(s). Default is "IN 7".
+- `stimulus_threshold`: A threshold value of type `T` for the stimulus. Default is 2.5.
+- `warn_bad_channel`: A `Bool` specifying whether to warn if a channel is improper. Default is `false`.
+- `flatten_episodic`: A `Bool` specifying whether to flatten episodic stimulation to be continuous. Default is `false`.
+- `time_unit`: A `Symbol` specifying the time unit. Default is `:s` (seconds).
 
-KWARGS:
-sweeps::Union{Int64,Vector{Int64}}
-    [DEFAULT, -1]
-    The sweeps that will be saved. By default -1 allows all sweeps to be read. However specific sweeps can be chosen
+# Returns
+- An `Experiment` object containing the extracted data, along with metadata.
 
-channels::Vector{String}
-    [DEFAULT, ["Vm_prime", "Vm_prime4"]] 
-    The channels that will be recorded. These can be specified as a string
-    By default, these are set to Vm_prime and Vm_prime4 which are voltage channels 0 and 4. 
-
-average_sweeps::Bool 
-    [DEFAULT, false]
-    Specifies whether or not the function will automatically average the sweeps. This can be useful in cases where multiple files are averaged
-
-stimulus_name::Union{String, Vector{String}}
-        [DEFAULT, "IN 7"]
-        This channel stores information about the stimulus. Note that this is different from the digital channel
-
-
+# Example
+```julia
+exp = readABF(Float32, "path/to/abf_file.abf")
+```
 """
 function readABF(::Type{T}, abf_data::Union{String,Vector{UInt8}};
     sweeps::Union{Int64,Vector{Int64}}=-1,
@@ -48,76 +114,35 @@ function readABF(::Type{T}, abf_data::Union{String,Vector{UInt8}};
     flatten_episodic::Bool=false, #If the stimulation is episodic and you want it to be continuous
     time_unit=:s, #The time unit is s, change to ms
 ) where {T<:Real}
+    # Read ABF header information
     HeaderDict = readABFInfo(abf_data)
-    #Pull out the requested channels
-    if isa(channels, Vector{String}) #If chs is a vector of channel names extract it as such
-        ch_idxs = findall(ch -> ch ∈ channels, HeaderDict["adcNames"])
-    elseif isa(channels, Vector{Int64}) #If chs is a vector of ints
-        ch_idxs = channels
-    elseif channels == -1 #if chs is -1 extract all channels
-        ch_idxs = HeaderDict["channelList"]
-    else
-        ch_idxs = channels
-    end
-    #Extract info for the adc names and units
-    ch_names = Vector{String}(HeaderDict["adcNames"][ch_idxs])
-    ch_units = Vector{String}(HeaderDict["adcUnits"][ch_idxs])
-    ch_telegraph = Vector{T}(HeaderDict["fTelegraphAdditGain"][ch_idxs])
-    #we can extract the data using getWaveform from above
-    if sweeps == -1 && channels == -1
-        data = HeaderDict["data"]
-    elseif sweeps == -1 && channels != -1
-        data = getWaveform(HeaderDict, ch_names; warn_bad_channel=warn_bad_channel)
-    elseif sweeps != -1 && channels == -1
-        data = HeaderDict["data"][sweeps, :, :]
-    elseif sweeps != -1 && channels != -1
-        data = getWaveform(HeaderDict, ch_names; warn_bad_channel=warn_bad_channel)
-        data = data[sweeps, :, :]
-    end
-    #println(size(data))
-    #We need to throw an error if a dimension is empty
-    if any(size(data) .== 0)
-        @warn begin
-            "There is in issue with the channels selected. 
-            Ensure you are picking one of the following channels:
-            $(HeaderDict["adcNames"]) 
-            or
-            $(HeaderDict["dacNames"])
-            "
-        end
-        throw(DimensionMismatch)
-    end
+
+    # Extract channel indices based on the input
+    ch_idxs = extract_channel_indices(channels, HeaderDict)
+
+    # Extract channel information
+    ch_names, ch_units, ch_telegraph = extract_channel_info(ch_idxs, HeaderDict)
+
+    # Extract data based on the specified sweeps and channels
+    data = extract_data(sweeps, channels, ch_names, HeaderDict, warn_bad_channel)
+
+    # Flatten episodic data if requested
     if flatten_episodic
-        n_size = size(data)
-        reshape_data = permutedims(data, (3, 2, 1))
-        reshape_data = reshape(reshape_data, 1, n_size[3], :)
-        data = permutedims(reshape_data, (1, 3, 2))
-    end
-    #println(size(data,2))
-    dt = HeaderDict["dataSecPerPoint"]
-    #There seems to be a bug
-    idxs = collect(0:size(data,2)-1)
-    t = idxs.*dt #Time is usually in seconds, but works better in ms
-    if time_unit == :ms
-        dt *= 1000
-        t .*= 1000
+        data = flatten_data(data)
     end
 
-    #This section we will rework to include getting analog and digital inputs
-    if isnothing(stimulus_name)
-        stimulus_protocol = StimulusProtocol("Nothing")
-    else
-        stimulus_protocol = extractStimulus(HeaderDict; stimulus_name = stimulus_name, stimulus_threshold = stimulus_threshold)
-    end
+    # Prepare time information
+    dt, t = prepare_time_info(data, HeaderDict, time_unit)
+
+    stimulus_protocol = extract_stimulus_protocol(HeaderDict, stimulus_name, stimulus_threshold)
+
+    # Average sweeps if requested
     if average_sweeps
-        data = sum(data, dims=1) / size(data, 1)
-        stimulus_protocol_first_vals = stimulus_protocol[1] 
-        stimulus_protocol = StimulusProtocol(stimulus_name) 
-        stimulus_protocol[1] = stimulus_protocol_first_vals
+        data, stimulus_protocol = average_data_and_protocol(data, stimulus_protocol)
     end
-    #With our new file structure we probably need to reorganize this a bit
-    return Experiment(HeaderDict, dt, t, data, ch_names, ch_units, ch_telegraph, stimulus_protocol)
-end
+    
+    # Return Experiment object
+    return Experiment(HeaderDict, dt, t, data, ch_names, ch_units, ch_telegraph, stimulus_protocol)end
 
 readABF(abf_path::Union{String,Vector{UInt8}}; kwargs...) = readABF(Float64, abf_path; kwargs...)
 
