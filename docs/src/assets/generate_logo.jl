627ca047c26dccae3c2eb665beb48d68e630d4c8
mattar13 <mtarchick13@gmail.com>
2023-04-11 19:12:48 -0400

    added logo

--- docs/src/assets/generate_logo.jl
+++ docs/src/assets/generate_logo.jl
@@ -2,18 +2,6 @@ using Pkg; Pkg.activate("")
 using Plots, Measures
 using Colors
 
-# Define a function to draw a spiral
-function spiral(t, a, b; beg = 0)
-     if t > beg
-          r = a + b * t
-          x = r * cos(t)
-          y = r * sin(t)
-          return x, y
-     else
-          return t, t
-     end
-end
-
 # Define a function to draw an action potential
 function action_potential(t)
     v = 0.0
@@ -31,6 +19,20 @@ function action_potential(t)
     return v
 end
 
+
+# Define a function to draw a spiral
+function spiral(t, a, b; beg = 0)
+    if t > beg
+        r = a + b * t
+        x = r * cos(t)
+        y = r * sin(t)
+        return x, y
+    else
+        return t, t
+    end
+end
+
+
 #%%
 # Define the parameters for the spiral and the action potential
 a = 10.0 # initial radius of the spiral
@@ -41,20 +43,21 @@ dt = 0.01 # step size for the angle
 vmin = -80.0 # minimum voltage of the action potential
 vmax = 80.0 # maximum voltage of the action potential
 
-# Generate the data points for the spiral and the action potential
+#%% Generate the data points for the spiral and the action potential
 t = tmin:dt:tmax # angle vector
 res = spiral.(t, a, b) # spiral coordinates
 xs = map(x -> x[1], res)
 ys = map(x -> x[2], res)
-vs = action_potential.(mod.(t, 2 * pi).-2.00) # action potential values
-minimum(vs)
-plot(xs, ys, vs+randn(size(vs)), 
-     line_z = vs, c = :julia_colorscheme, legend=false, lw = 5, 
-     camera =(0,80), 
-     showaxis = false, grid = false, size = (500.0, 500.0), 
-     margin = -100.0mm
-     )
+vs = action_potential.(mod.(t, 2pi))./70.0# 2 * piaction potential values
+logo = plot(xs, ys, vs,#+randn(size(vs))*0.5, 
+    line_z = vs, 
+    c = :julia_colorscheme, legend=false, lw = 10, 
+    camera =(0,80), 
+    showaxis = false, grid = false, size = (500.0, 500.0), 
+    margin = -200.0mm, dpi = 600
+    )
 #plot!(xs, ys, zeros(length(xs)).-70.0, c = :black, lw = 2.0)
-title!("ElectroPhysiology.jl", titlefontsize = 25, titlefontfamily = "MN Latin", )
-     #zlabel!("ElectroPhysiology.jl")
+title!("ElectroPhysiology.jl", titlefontsize = 20, titlefontfamily = "MN Latin", )
+savefig(logo, raw"docs\src\assets\ElectroPhysiologyLogo.png")
+#zlabel!("ElectroPhysiology.jl")
 #ylabel!("A Julia package for electrophysiology")
