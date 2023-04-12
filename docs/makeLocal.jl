4d1cc01a755b16ba804a6899d179cd2a62fe39cc
mattar13 <mtarchick13@gmail.com>
2023-04-12 15:28:29 -0400

    added a make local

---
+++ docs/makeLocal.jl
@@ -0,0 +1,22 @@
+push!(LOAD_PATH, "../src/")
+using Pkg; Pkg.activate("docs")
+using Documenter 
+using ElectroPhysiology 
+#using PhysiologyAnalysis #Comment this out because it uses pyimport which I still haven't configured yet
+import ElectroPhysiology: Experiment
+
+makedocs(
+     sitename = "ElectroPhysiology.jl", 
+     authors = "Matthew Tarchick", 
+     repo = "https://github.com/mattar13/ElectroPhysiology.jl",
+     pages = Any[
+          "Introduction" => "index.md",
+          "Installation" => "installation.md",
+          "Tutorial" => "tutorial.md",
+          "Functions" => "functions.md"
+     ],
+     #=
+     modules = [ElectroPhysiology, PhysiologyAnalysis],
+     format = Documenter.HTML()
+     =#
+)
