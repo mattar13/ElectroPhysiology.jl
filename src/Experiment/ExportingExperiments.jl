ec68415cc0cba2efd0ceb7e4576b37da6358c44d
mattar13 <mtarchick13@gmail.com>
2023-05-13 13:04:33 -0400

    added writeCSV function

--- src/Experiment/ExportingExperiments.jl
+++ src/Experiment/ExportingExperiments.jl
@@ -1,5 +1,24 @@
-function get_experiment_information()
-     
+function writeCSV(exp::Experiment; loc = nothing, channel = nothing)
+     if isnothing(channel)
+          for ch in eachchannel(exp)
+               a = ch.data_array[:,:,1]' #We have to transpose the data to store it
+               if isnothing(loc)
+                    writedlm("$(ch.chNames[1]).csv", a, ',')
+               else
+                    writedlm("$(loc)/$(ch.chNames[1]).csv", a, ',')
+               end
+          end
+     elseif isa(channel, Int64)
+          a = data[:,:,channel]
+          if isnothing(loc)
+               writedlm("$(ch.chNames[1]).csv", a, ',')
+          else
+               writedlm("$(loc)/$(ch.chNames[1]).csv", a, ',')
+          end
+     end
+end
 
+function readCSVtoExperiment(filename::String, exp::Experiment, channel)
+     
 
 end
