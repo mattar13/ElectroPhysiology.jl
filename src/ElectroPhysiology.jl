a2fe8364e01444e53925c3b59caecc7f17ff4cbe
mattar13 <mtarchick13@gmail.com>
2023-08-29 15:25:41 -0400

    removed some exports

--- src/ElectroPhysiology.jl
+++ src/ElectroPhysiology.jl
@@ -14,7 +14,6 @@ using XLSX, DataFrames
 
 include("Experiment/StimulusProtocol.jl")
 export StimulusProtocol
-export setindex!, getindex
 export extractStimulus, setIntensity, getIntensity
 
 include("Experiment/Experiments.jl") #This file contains the Experiment structure. 
