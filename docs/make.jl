d97b812a82a67e83851717b1daed8ccd703a0c1a
mattar13 <mtarchick13@gmail.com>
2023-09-18 16:44:04 -0400

    updated documentation

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,8 @@ using ElectroPhysiology
 using PhysiologyAnalysis #Comment this out because it uses pyimport which I still haven't configured yet
 using PhysiologyPlotting
 using PyPlot #Export this as a dependancy
+
+import PhysiologyPlotting: add_scalebar, add_sig_bar, draw_axes_border
 #using GLMakie #OpenGL is not available, so this may be left out for now
 import ElectroPhysiology: Experiment, StimulusProtocol, extractStimulus, setIntensity, getIntensity
 import ElectroPhysiology: Stimulus, Flash
