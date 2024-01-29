8620b6fd95df81782ef13d619b99a4ae217a9e6e
mattar13 <mtarchick13@gmail.com>
2024-01-29 15:34:14 -0800

    revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -2,3 +2,24 @@
 using Revise
 using ElectroPhysiology
 import ElectroPhysiology as EP
+
+#%% I want to try to see if we can add a channel for a stimulus waveform
+
+#The data file exists here
+root = raw"E:\Data\Patching"
+file = "2024_01_25_ChAT-RFP_DSGC/Cell1/24125000.abf"
+filename = joinpath(root, file)
+
+#get the abf info first
+data = readABF(filename)
+
+wvform = EP.create_signal_waveform(data, "Analog 0")
+wvform.chNames
+wvform.t
+
+EP.create_signal_waveform!(data, "Analog 0")
+data.chNames
+data.data_array
+
+
+#%% Now we begin a new venture. Opening images as files
