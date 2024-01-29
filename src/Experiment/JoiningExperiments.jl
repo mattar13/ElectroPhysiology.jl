6a4c3d030a291ec9ad41b4f55578921d9430ba08
mattar13 <mtarchick13@gmail.com>
2024-01-29 15:33:44 -0800

    made cat and concat more efficcient

--- src/Experiment/JoiningExperiments.jl
+++ src/Experiment/JoiningExperiments.jl
@@ -1,82 +1,81 @@
-"""
-    concat(data::Experiment{T}, data_add::Experiment{T}; kwargs...) where {T}
-    concat!(data::Experiment{T}, data_add::Experiment{T}; kwargs...) where {T}
+import Base: cat, hcat, vcat
+function cat(exp::Experiment, a::AbstractArray; dims = 1)
+    new_exp = deepcopy(exp)
+    new_exp.data_array = cat(exp.data_array, a; dims = dims)
+    new_exp.chNames = [exp.chNames..., "new channel"]
+    new_exp.chUnits = [exp.chUnits..., "new units"]
+    new_exp.chTelegraph = [exp.chTelegraph..., 1.0]
+    return new_exp
+end
 
-Return a new `Experiment` object with `data` concatenated with `data_add` along the trials dimension.
+channelcat(exp::Experiment, a::AbstractArray) = cat(exp, a; dims = 3)
+trialcat(exp::Experiment, a::AbstractArray) = cat(exp, a; dims = 1)
+
+"""
+    concat(exp::Experiment{T}, exp_add::Experiment{T}; kwargs...) where {T}
+    concat!(exp::Experiment{T}, exp_add::Experiment{T}; kwargs...) where {T}
+
+Return a new `Experiment` object with `exp` concatenated with `exp_add` along the trials dimension.
 
 # Arguments
-- `data`: An `Experiment` object containing the experimental data.
-- `data_add`: Another `Experiment` object to be concatenated with the `data`.
+- `exp`: An `Experiment` object containing the experimental exp.
+- `exp_add`: Another `Experiment` object to be concatenated with the `exp`.
 
 # Keyword Arguments
-- `mode`: Specifies whether to pad or chop the data if the sizes do not match (default: `:pad`).
+- `mode`: Specifies whether to pad or chop the exp if the sizes do not match (default: `:pad`).
 - `position`: Specifies the position to pad or chop (default: `:post`).
 - `verbose`: Prints information if set to true (default: false).
 - `channel_mode`: Specifies how to handle extra channels (default: `:remove_extra`).
 
 # Example
 ```julia
-exp1 = Experiment(data_array1)
-exp2 = Experiment(data_array2)
+exp1 = Experiment(exp_array1)
+exp2 = Experiment(exp_array2)
 concatenated_exp = concat(exp1, exp2)
 ```julia
-exp1 = Experiment(data_array1)
-exp2 = Experiment(data_array2)
+exp1 = Experiment(exp_array1)
+exp2 = Experiment(exp_array2)
 concat!(exp1, exp2)
 ```
 """
-function concat(data::Experiment{T}, data_add::Experiment{T}; mode::Symbol=:pad, position::Symbol=:post, kwargs...) where {T}
-    new_data = deepcopy(data)
-    concat!(new_data, data_add)
-    return new_data
-end
 
-function concat!(data::Experiment{T}, data_add::Experiment{T}; 
-        mode::Symbol=:pad, position::Symbol=:post, verbose=false, 
-        channel_mode::Symbol = :remove_extra,
-        kwargs...) where {T}
-    if size(data, 2) > size(data_add, 2)
-        #println("Original data larger $(size(data,2)) > $(size(data_add,2))")
-        n_vals = abs(size(data, 2) - size(data_add, 2))
-        if mode == :pad
-            pad!(data_add, n_vals; position=position)
-        elseif mode == :chop
-            chop!(data, n_vals; position=position)
-        end
-    elseif size(data, 2) < size(data_add, 2)
-        #println("Original data smaller $(size(data,2)) < $(size(data_add,2))")
-        n_vals = abs(size(data, 2) - size(data_add, 2))
-        if mode == :pad
-            pad!(data, n_vals; position=position)
-        elseif mode == :chop
-            chop!(data_add, n_vals; position=position)
+function concat!(exp::Experiment, exp2::Experiment;
+    dims = 1, mode = :pad, position::Symbol=:post,
+)
+    if mode == :strict
+        @assert size(exp) == size(exp_add)
+    elseif mode == :pad || mode == :chop
+        if size(exp, 2) > size(exp2, 2)
+            #println("Original exp larger $(size(exp,2)) > $(size(exp_add,2))")
+            n_vals = abs(size(exp, 2) - size(exp_add, 2))
+            if mode == :pad
+                pad!(exp2, n_vals; position=position)
+            elseif mode == :chop
+                chop!(exp, n_vals; position=position)
+            end
+        elseif size(exp, 2) < size(exp2, 2)
+            #println("Original exp smaller $(size(exp,2)) < $(size(exp_add,2))")
+            n_vals = abs(size(exp, 2) - size(exp_add, 2))
+            if mode == :pad
+                pad!(exp, n_vals; position=position)
+            elseif mode == :chop
+                chop!(exp2, n_vals; position=position)
+            end
         end
     end
-
-    if size(data, 3) != size(data_add, 3) #This means the cannels do not match
-        if verbose
-            println("Concatenated file has too many channels")
-            println(data_add.chNames)
-        end
-        #We want to remove channels that are hanging. 
-        ch1in2 = indexin(data.chNames, data_add.chNames)
-        ch2in1 = indexin(data_add.chNames, data.chNames)
-        if any(isnothing.(ch1in2))
-            hanging_channels = findall(isnothing.(ch1in2))
-            data_dropped = drop(data, dim = 3, drop_idx = hanging_channels[1])
-            push!(data_dropped, data_add)
-            push!(data_dropped.stimulus_protocol, data_add.stimulus_protocol)
-        elseif any(isnothing.(ch2in1))
-            hanging_channels = findall(isnothing.(ch2in1))
-            data_dropped = drop(data_add, dim = 3, drop_idx = hanging_channels[1])
-            push!(data, data_dropped)
-            push!(data.stimulus_protocol, data_dropped.stimulus_protocol)
-        end
-    else
-        push!(data, data_add)
-        push!(data.stimulus_protocol, data_add.stimulus_protocol)
-    end
+    exp.data_array = cat(exp.data_array, exp2.data_array, dims = dims)
+    exp.chNames = [exp.chNames..., exp2.chNames...]
+    exp.chUnits = [exp.chUnits..., exp.chUnits...]
+    exp.chTelegraph = [exp.chTelegraph..., exp.chTelegraph...]
+    return 
 end
+
+function concat(exp::Experiment{T}, exp_add::Experiment{T}; mode::Symbol=:pad, position::Symbol=:post, kwargs...) where {T}
+    new_exp = deepcopy(exp)
+    concat!(new_exp, exp_add)
+    return new_exp
+end
+
 
 """
     match_channels(exp1::Experiment, exp2::Experiment)
@@ -87,7 +86,7 @@ If the number of channels in `exp1` and `exp2` is not equal, this function will 
 from the `Experiment` object with more channels, making both objects have the same number of channels.
 
 # Arguments
-- `exp1`: An `Experiment` object containing the experimental data.
+- `exp1`: An `Experiment` object containing the experimental exp.
 - `exp2`: Another `Experiment` object to be matched with `exp1`.
 
 # Returns
@@ -95,8 +94,8 @@ from the `Experiment` object with more channels, making both objects have the sa
 
 # Example
 ```julia
-exp1 = Experiment(data_array1)
-exp2 = Experiment(data_array2)
+exp1 = Experiment(exp_array1)
+exp2 = Experiment(exp_array2)
 exp1_modified, exp2_modified = match_channels(exp1, exp2)
 ```
 """
@@ -117,38 +116,44 @@ end
 """
     sub_exp(exp1::Experiment, exp2::Experiment)
 
-Subtract the data of `exp2` from `exp1` and return a new `Experiment` object with the resulting data.
+Subtract the exp of `exp2` from `exp1` and return a new `Experiment` object with the resulting exp.
 
 This function will automatically match the channels of both `Experiment` objects if they do not match. 
 If the channels do not match, it will drop the unmatching channels by default.
 
 # Arguments
-- `exp1`: An `Experiment` object containing the experimental data.
-- `exp2`: Another `Experiment` object containing the data to be subtracted from `exp1`.
+- `exp1`: An `Experiment` object containing the experimental exp.
+- `exp2`: Another `Experiment` object containing the exp to be subtracted from `exp1`.
 
 # Returns
-- `data`: A new `Experiment` object with the resulting data after the subtraction.
+- `exp`: A new `Experiment` object with the resulting exp after the subtraction.
 
 # Example
 ```julia
-exp1 = Experiment(data_array1)
-exp2 = Experiment(data_array2)
+exp1 = Experiment(exp_array1)
+exp2 = Experiment(exp_array2)
 result_exp = sub_exp(exp1, exp2)
 ```
 """
 function sub_exp(exp1::Experiment, exp2::Experiment)
     if size(exp1) == size(exp2)
-        data = deepcopy(exp1)
+        exp = deepcopy(exp1)
         #return a new experiment? make a new exp
-        data.data_array = exp1.data_array - exp2.data_array
-        return data
+        exp.exp_array = exp1.exp_array - exp2.exp_array
+        return exp
     else #If the channels don't match, we will automatically drop the unmatching one by default
         exp1, exp2 = match_channels(exp1, exp2)
-        data = deepcopy(exp1)
+        exp = deepcopy(exp1)
         #return a new experiment? make a new exp
-        data.data_array = exp1.data_array - exp2.data_array
-        return data
+        exp.exp_array = exp1.exp_array - exp2.exp_array
+        return exp
     end
 end
 
 -(exp1::Experiment, exp2::Experiment) = sub_exp(exp1, exp2)
+
+function create_signal_waveform!(exp::Experiment, channel::String)
+    wvform = getWaveform(exp.HeaderDict, channel)
+    cat_exp = Experiment(exp.t, wvform)
+    concat!(exp, cat_exp)
+end
