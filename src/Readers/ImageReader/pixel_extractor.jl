1dc4f0ac6376bf6205dba58c037a0a5de961d85c
mattar13 <mtarchick13@gmail.com>
2024-02-01 13:57:06 -0800

    deleted pixel extractor

--- src/Readers/ImageReader/pixel_extractor.jl
+++
@@ -1,5 +0,0 @@
-#I want to keep with the ethos that the only programs that go into this package 
-#Are functions that result in Experiments being returned. Therefore these may go in PhysiologyAnalysis.jl
-
-
-
