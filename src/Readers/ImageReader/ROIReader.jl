efdaca9ecbadbd4a683ee829795ecedd5146c8f6
mattar13 <mtarchick13@gmail.com>
2024-02-13 13:31:15 -0800

    add ROI loading

--- src/Readers/ImageReader/ROIReader.jl
+++ src/Readers/ImageReader/ROIReader.jl
@@ -1,11 +1,3 @@
-"""
-ROIs are masks. 
-A ROI is a mask of falses (which are not included in the image) and trues (which are)
-
-"""
-function recordROI(exp::Experiment{TWO_PHOTON, T}, roi_idxs::Vector{Int64}, label::Int64) where T <: Real 
-     exp.HeaderDict["ROIs"][roi_idxs] .= label
-end
 
 
 getROIindexes(exp::Experiment{TWO_PHOTON, T}, label::Int64) where T<:Real = findall(exp.HeaderDict["ROIs"] .== label)
@@ -40,4 +32,25 @@ function getROIarr(data::Experiment{TWO_PHOTON, T}) where T<:Real
           push!(ROI_exps, data_ROI)
      end
      return ROI_exps
+end
+
+"""
+ROIs are masks. 
+A ROI is a mask of falses (which are not included in the image) and trues (which are)
+
+"""
+function recordROI(exp::Experiment{TWO_PHOTON, T}, roi_idxs::Vector{Int64}, label::Int64) where T <: Real 
+     exp.HeaderDict["ROIs"][roi_idxs] .= label
+end
+
+#Make a new function 
+function loadROIfn(fn::String, exp::Experiment{TWO_PHOTON, T}) where T<:Real
+     ROI_mask_img = load(fn)
+     ROI_mask_gray = Gray.(ROI_mask_img)
+     ROI_mask_gray = reshape(ROI_mask_gray, length(ROI_mask_gray)) |> vec
+     for (mask_idx, mask_val) in enumerate(unique(ROI_mask_gray)[2:end])
+         ROI_mask_indexes = findall(ROI_mask_gray .== mask_val)
+         recordROI(exp, ROI_mask_indexes, mask_idx)
+     end
+     exp.HeaderDict["ROIs"]
 end
