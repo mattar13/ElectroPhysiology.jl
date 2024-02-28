5c3311582e2510a242134101bd1156a4f2109c85
mattar13 <mtarchick13@gmail.com>
2024-02-27 16:03:41 -0800

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,50 +1,26 @@
+using Pkg
+#%% Do any revisions to the base package (without Requires) here
 using Revise
 using ElectroPhysiology
-import ElectroPhysiology as EP
+#Change how stimulus protocol
+
+
+#%% If you want to test packages that are included in Requires (or any other package) test them here (Run above code)
+test_workspace_fn = raw"C:\Users\mtarc\.julia\dev\ElectroPhysiology\test"
+Pkg.activate(test_workspace_fn)
+using MAT
+
+#%% Now we can load the MAT package to look at the data
 ElectroPhysiology.__init__()
-using Pkg; Pkg.activate("test")
-using FileIO, ImageView, Images
+readMAT("test\\eWT_PD.mat")
+
+a = rand(1, 4500)
+b = rand(1, 4500)
+trials = [a, b]
+cat(trials..., dims = 3)
+#%% Now we want to load and run the 
+#%% Run things in the testing environment
+
+
 using GLMakie
 using Statistics
-
-root = raw"F:\Data\Calcium Images\2024_01_31_FRMD7_Cal590"
-file = "2024_01_31_ROI004.tif"
-fn = joinpath(root, file)
-data = readImage(fn)
-mov = get_all_frames(data) #get all frames as a 3D array
-zproj = mean(mov, dims = 3)[:,:,1] #Can we save this directly? 
-
-#%% Figure out how to load a mask image as a ROI array
-fn = raw"C:\Users\mtarc\.julia\dev\ElectroPhysiology\test\2024_02_09_18_06_27.654700test_img_masks.png"
-loadROIfn!(fn, data)
-ROI_mask = getROImask(data)
-
-px_x, px_y = data.HeaderDict["framesize"]
-xlims = 1:px_x
-ylims = 1:px_y
-zlims = 1:size(data,2)
-#%% Lets make a plot that shows a few of the ROIs
-fig = GLMakie.Figure(size = (1000, 500))
-#Plot the projected image and the mask
-ax11 = GLMakie.Axis(fig[1,1])
-ax12 = GLMakie.Axis(fig[1,2])
-heatmap!(ax11, xlims, ylims, zproj, colormap = Reverse(:algae))
-heatmap!(ax12, xlims, ylims, ROI_mask, colormap = Reverse(:algae))
-
-# Extract the ROI movie array
-ROI1_mov = getROIarr(data, 21)
-ROIall_mov = getROIarr(data)
-ax21 = GLMakie.Axis(fig[2,1])
-ax22 = GLMakie.Axis(fig[2,2])
-hm1 = heatmap!(ax21, xlims, ylims, mov[:,:,1], colormap = Reverse(:algae), colorrange = (0.0, 0.05))
-hm2 = heatmap!(ax22, xlims, ylims, ROIall_mov[:,:,1], colormap = Reverse(:algae), colorrange = (0.0, 0.05))
-
-#Convert the centroid of each cell coordinate to an X,Y coordinate
-display(fig)
-#%%
-record(fig, "ANIMATION.mp4", 2:size(data,2)) do i 
-     println(i)
-     hm1[3] = mov[:,:,i]
-     hm2[3] = ROIall_mov[:,:,i]
-end
-display(fig)
