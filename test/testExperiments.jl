cfb5921632d81c05673ce26e794b2515f8302791
mattar13 <mtarchick13@gmail.com>
2024-02-28 11:14:29 -0800

    updated experiment tests

---
+++ test/testExperiments.jl
@@ -0,0 +1,10 @@
+@testset "Testing Experiement struct" begin
+     time = collect(1:0.01:1000) #Create some time range
+     data = rand(5, length(time), 1) #Create some dummy data
+ 
+     exp = ElectroPhysiology.Experiment(data)
+     @test !isnothing(exp)
+ 
+     exp_w_time = ElectroPhysiology.Experiment(time, data)
+     @test !isnothing(exp_w_time)
+ end
