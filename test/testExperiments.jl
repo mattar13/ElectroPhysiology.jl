52c313f5d097f44c670fdc4f862da47890886911
mattar13 <mtarchick13@gmail.com>
2024-02-28 11:43:56 -0800

    updated experiment test

--- test/testExperiments.jl
+++ test/testExperiments.jl
@@ -1,10 +1,21 @@
-@testset "Testing Experiement struct" begin
+@testset "Constructing experiments" begin
      time = collect(1:0.01:1000) #Create some time range
-     data = rand(5, length(time), 1) #Create some dummy data
- 
-     exp = ElectroPhysiology.Experiment(data)
-     @test !isnothing(exp)
- 
-     exp_w_time = ElectroPhysiology.Experiment(time, data)
-     @test !isnothing(exp_w_time)
- end
+     data_arr = rand(5, length(time), 5) #Create some dummy data
+     experiment = ElectroPhysiology.Experiment(data_arr)
+     @test isa(experiment, ElectroPhysiology.Experiment)
+     
+     experiment_w_time = ElectroPhysiology.Experiment(time, data_arr)
+     @test isa(experiment_w_time, ElectroPhysiology.Experiment)
+end
+
+@testset "Iterating through experiments" begin
+     time = collect(1:0.01:1000) #Create some time range     
+     data_arr = rand(5, length(time), 5) #Create some dummy data
+     experiment = ElectroPhysiology.Experiment(data_arr)
+     @test isa(experiment, ElectroPhysiology.Experiment)
+
+     println(experiment.chNames)
+     exp_get_1 = getdata(experiment, 1, 1, 1)
+
+
+end
