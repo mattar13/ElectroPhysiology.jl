939fcd30559b209b81cfbe21d410f607ad0adde7
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-22 01:25:20 -0700

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -8,24 +8,3 @@ cell_img_fn = raw"D:\Data\Calcium Imaging\2024_05_23_MORF_ChATCre\ca_img_5011.ti
 ElectroPhysiology.__init__()
 @time ca_img_exp = readImage(cell_img_fn);
 @time getIMG_datetime(cell_img_fn);
-ca_img_exp.HeaderDict
-ca_img_exp.HeaderDict["date:create"]
-
-getIMG_datetime(cell_img_fn)
-for (k,v) in ca_img_exp.HeaderDict
-    if k != "ROI"
-        println("$k -> $v")
-    end
-end
-
-ca_img_exp.HeaderDict["state.internal"]
-
-#%%
-using Dates
-
-deinterleave!(data) #This seperates the movies into two seperate movies
-
-img_arr = get_all_frames(data)
-green_ch = project(data, dims = 3)[:,:,1,1]
-green_ch = project(data, dims = 3)[:,:,1,2]
-fluo_project = project(data, dims = (1,2))[1,1,:,1]
