70c6a03d9669f7d23d2f0d8157a5dc682d66a8be
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-21 23:52:57 -0700

    added ImageMagick to test package

--- test/revisions.jl
+++ test/revisions.jl
@@ -2,12 +2,17 @@ using Revise
 using ElectroPhysiology
 import ElectroPhysiology.convert_stimulus!
 using Pkg; Pkg.activate("test")
-using ImageView, FileIO, Images
+using ImageView, FileIO, Images, ImageMagick
 cell_img_fn = raw"D:\Data\Calcium Imaging\2024_05_23_MORF_ChATCre\ca_img_5011.tif"
 
-data = readImage(cell_img_fn)
-deinterleave!(data) #This seperates the movies into two seperate movies
 ElectroPhysiology.__init__()
+data = readImage(cell_img_fn)
+data.HeaderDict
+data.HeaderDict["date:create"]
+#%%
+ using Dates
+
+deinterleave!(data) #This seperates the movies into two seperate movies
 
 img_arr = get_all_frames(data)
 green_ch = project(data, dims = 3)[:,:,1,1]
