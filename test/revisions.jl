31aeb99967833549efa3a9366832ee0a592e1a0c
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-22 00:45:08 -0700

    updated tests

--- test/revisions.jl
+++ test/revisions.jl
@@ -6,11 +6,21 @@ using ImageView, FileIO, Images, ImageMagick
 cell_img_fn = raw"D:\Data\Calcium Imaging\2024_05_23_MORF_ChATCre\ca_img_5011.tif"
 
 ElectroPhysiology.__init__()
-data = readImage(cell_img_fn)
-data.HeaderDict
-data.HeaderDict["date:create"]
+ca_img_exp = readImage(cell_img_fn)
+ca_img_exp.HeaderDict
+ca_img_exp.HeaderDict["date:create"]
+
+
+for (k,v) in ca_img_exp.HeaderDict
+    if k != "ROI"
+        println("$k -> $v")
+    end
+end
+
+ca_img_exp.HeaderDict["state.internal"]
+
 #%%
- using Dates
+using Dates
 
 deinterleave!(data) #This seperates the movies into two seperate movies
 
