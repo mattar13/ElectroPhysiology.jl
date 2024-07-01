38ac8b674b9e9ebe9b75285dddac550281895ad4
mattar13 <mtarchick13@gmail.com>
2024-07-01 11:09:14 -0700

    updated documents

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,10 @@
 push!(LOAD_PATH, "../src/")
 using Pkg; Pkg.activate("docs")
-#We need to build PyCall before using this to properly use
-ENV["PYTHON"]=""
-Pkg.build("PyCall")
 
 using Documenter 
-using ElectroPhysiology 
-using PhysiologyAnalysis #Comment this out because it uses pyimport which I still haven't configured yet
+using ElectroPhysiology, PhysiologyAnalysis #Comment this out because it uses pyimport which I still haven't configured yet
 using PhysiologyPlotting
-using PyPlot #Export this as a dependancy
+
 
 import PhysiologyPlotting: add_scalebar, add_sig_bar, draw_axes_border
 #using GLMakie #OpenGL is not available, so this may be left out for now
