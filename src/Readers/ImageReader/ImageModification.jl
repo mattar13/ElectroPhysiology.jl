562fab902f441696e8de8146e37f89e74e926ec8
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-08-05 21:25:41 -0700

    added the ability to BC adjust by channel (almost never can do both channels)

--- src/Readers/ImageReader/ImageModification.jl
+++ src/Readers/ImageReader/ImageModification.jl
@@ -1,43 +1,45 @@
-function adjustBC!(exp; 
-     min_val_y = 0.0, max_val_y = 1.0, 
-     min_val_x = :auto, max_val_x = :auto,
+function adjustBC!(exp; channel = nothing,
+     min_val_y = 0.0, max_val_y = 1.0, std_level = 1,
+     min_val_x = :std, max_val_x = :std,
      contrast = :auto, brightness = :auto,
-     n_vals = 10
+     n_vals = 10, 
 )
      for (i, ch) in enumerate(eachchannel(exp))
-          if min_val_x == :auto
-               min_val_x = minimum(ch)
-          elseif min_val_x == :std
-               mean_val = mean(ch, dims = (1,2))[1,1,1]
-               std_val = std(ch, dims = (1,2))[1,1,1]
-               min_val_x = mean_val - std_val
-          elseif min_val_x == :ci
-               mean_val = mean(ch, dims = (1,2))[1,1,1]
-               std_val = std(ch, dims = (1,2))[1,1,1]
-               min_val_x = mean_val - std_val/sqrt(length(ch))
-          end
+          if isnothing(channel) || channel == i
+               println("BC this channel: $(ch.chNames[1])")
+               if min_val_x == :auto
+                    min_val_x = minimum(ch)
+               elseif min_val_x == :std
+                    mean_val = mean(ch, dims = (1,2))[1,1,1]
+                    std_val = std_level*std(ch, dims = (1,2))[1,1,1]
+                    min_val_x = mean_val - std_val
+               elseif min_val_x == :ci
+                    mean_val = mean(ch, dims = (1,2))[1,1,1]
+                    std_val = std_level*std(ch, dims = (1,2))[1,1,1]
+                    min_val_x = mean_val - std_val/sqrt(length(ch))
+               end
 
-          if max_val_x == :auto
-               max_val_x = maximum(ch)
-          elseif max_val_x == :std
-               mean_val = mean(ch, dims = (1,2))[1,1,1]
-               std_val = std(ch, dims = (1,2))[1,1,1]
+               if max_val_x == :auto
+                    max_val_x = maximum(ch)
+               elseif max_val_x == :std
+                    mean_val = mean(ch, dims = (1,2))[1,1,1]
+                    std_val = std_level*std(ch, dims = (1,2))[1,1,1]
+                    max_val_x = mean_val + std_val
+               elseif max_val_x == :ci
+                    mean_val = mean(ch, dims = (1,2))[1,1,1]
+                    std_val = std_level*std(ch, dims = (1,2))[1,1,1]
+                    max_val_x = mean_val + std_val/sqrt(length(ch))
+               end
 
-               max_val_x = mean_val + std_val
-          elseif max_val_x == :ci
-               mean_val = mean(ch, dims = (1,2))[1,1,1]
-               std_val = std(ch, dims = (1,2))[1,1,1]
-               max_val_x = mean_val + std_val/sqrt(length(ch))
+               if contrast == :auto
+                    input_range = LinRange(min_val_x, max_val_x, n_vals)
+                    output_range = LinRange(min_val_y, max_val_y, n_vals)
+                    lin_fit = curve_fit(LinearFit, input_range, output_range)
+               else
+                    lin_fit(x) = contrast*x + brightness
+               end
+               exp.data_array[:,:,i] .= lin_fit.(ch.data_array)
           end
-
-          if contrast == :auto
-               input_range = LinRange(min_val_x, max_val_x, n_vals)
-               output_range = LinRange(min_val_y, max_val_y, n_vals)
-               lin_fit = curve_fit(LinearFit, input_range, output_range)
-          else
-               lin_fit(x) = contrast*x + brightness
-          end
-          exp.data_array[:,:,i] .= lin_fit.(ch.data_array)
      end
      return exp
 end
--- test/revisions.jl
+++ test/revisions.jl
@@ -4,10 +4,10 @@ using Dates, TimeZones
 using ElectroPhysiology
 using Pkg; Pkg.activate("test")
 using GLMakie, PhysiologyPlotting
-using StatsBase
+using StatsBase #Might need to add this to PhysiologyAnalysis as well
 
 #%% There is something wrong with the timestamps
-data2P_fn = raw"G:\Data\Calcium Imaging\2024_07_22_OPN4g_P7\cell_img1002.tif"
+data2P_fn = raw"D:\Data\Calcium Imaging\2024_07_22_OPN4g_P7\cell_img1002.tif"
 fig = Figure(size = (600, 600))
 ax1a = GLMakie.Axis(fig[1,1], aspect = 1.0)
 ax2a = GLMakie.Axis(fig[2,1])
@@ -26,7 +26,10 @@ image!(ax1a, red_img)
 lines!(ax2a, red_z_fit.edges[1][2:end], red_z_fit.weights)
 xlims!(ax2a, 0.0, 1.0)
 
-adjustBC!(data2P, min_val_x = :ci, max_val_x = :ci, max_val_y = 1.0)
+mu = mean(data2P, dims = (1,2))[1,1,1]
+sig = std(data2P, dims = (1,2))[1,1,1]
+adjustBC!(data2P, channel = 1, min_val_x = mu-0.05sig, max_val_x = mu+0.1sig)
+#%%
 red_zproj = project(data2P, dims = (3))[:,:,1,1]
 red_img = RGBf.(red_zproj, zeros(size(red_zproj)), zeros(size(red_zproj)))
 red_z_fit = fit(Histogram, red_zproj |> vec, LinRange(0.0, 1.0, 1000))
