d58ddea4f603a23d7c7f3491a4f0c9070a419dbe
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-08-14 21:59:31 -0700

    update version

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,37 +1,70 @@
 using Revise
 using Pkg; Pkg.activate(".")
-using Dates, TimeZones
 using ElectroPhysiology
 using Pkg; Pkg.activate("test")
 using GLMakie, PhysiologyPlotting
 using StatsBase #Might need to add this to PhysiologyAnalysis as well
 
-#%% There is something wrong with the timestamps
-data2P_fn = raw"D:\Data\Calcium Imaging\2024_07_22_OPN4g_P7\cell_img1002.tif"
-fig = Figure(size = (600, 600))
-ax1a = GLMakie.Axis(fig[1,1], aspect = 1.0)
-ax2a = GLMakie.Axis(fig[2,1])
-ax1b = GLMakie.Axis(fig[1,2], aspect = 1.0)
-ax2b = GLMakie.Axis(fig[2,2])
+using GLMakie, PhysiologyPlotting
 
-# ╔═╡Extract the image
+data2P_fn = "D:/Data/Two Photon/2024_08_08_OPN4_P20_SWCNT/light_stim003.tif"#might be good
+
+#╔═╡Extract the image
 data2P = readImage(data2P_fn);
-deinterleave!(data2P)
+xlims = data2P.HeaderDict["xrng"]
+ylims = data2P.HeaderDict["yrng"]
+truncate_data!(data2P, t_begin = 200.0, t_end = 400.0)
+deinterleave!(data2P) #This seperates the movies into two seperate movies
+img_arr = get_all_frames(data2P)
+#Extract the objects
+red_zstack = img_arr[:,:,:,2]
+red_zproj = project(data2P, dims = (3))[:,:,1,2]
+red_trace = project(data2P, dims = (1,2))[1,1,:,2]
 
-red_zproj = project(data2P, dims = (3))[:,:,1,1]
-red_img = RGBf.(red_zproj, zeros(size(red_zproj)), zeros(size(red_zproj)));
-red_z_fit = fit(Histogram, red_zproj |> vec, LinRange(0.0, 1.0, 1000));
+#Give it a mean filter
+kernel = ones(1,1,20, 1)
+img_filt = imfilter(img_arr, kernel)
+#Extract the objects
+f_red_zstack = img_filt[:,:,:,2]
+f_red_zproj = project(data2P, dims = (3))[:,:,1,2]
+f_red_trace = project(data2P, dims = (1,2))[1,1,:,2]
 
-mu = mean(data2P, dims = (1,2))[1,1,1]
-sig = std(data2P, dims = (1,2))[1,1,1]
-adjustBC!(data2P, channel = 1, min_val_x = mu-0.05sig, max_val_x = mu+0.1sig)
+#%% Plot the figure
+fig = Figure(size = (1000, 800))
+ax1b = GLMakie.Axis(fig[2,1], title = "Red Channel", aspect = 1.0)
 
-red_zproj = project(data2P, dims = (3))[:,:,1,1]
-red_img = RGBf.(red_zproj, zeros(size(red_zproj)), zeros(size(red_zproj)))
-red_z_fit = fit(Histogram, red_zproj |> vec, LinRange(0.0, 1.0, 1000))
+#%%
+fig = Figure(size = (1000, 800))
+ax1a = GLMakie.Axis(fig[1,1], title = "Green Channel", aspect = 1.0)
+ax1b = GLMakie.Axis(fig[2,1], title = "Red Channel", aspect = 1.0)
 
-image!(ax1b, red_img)
-lines!(ax2b, red_z_fit.edges[1][2:end], red_z_fit.weights)
-xlims!(ax2a, 0.0, 1.0)
+ax2a = GLMakie.Axis(fig[1,2], title = "Green Trace")#, aspect = 1.0)
+ax2b = GLMakie.Axis(fig[2,2], title = "Red Trace")#, aspect = 1.0)
 
+mu_grn = mean(grn_zstack)
+sig_grn = std(grn_zstack)*2
+
+mu_red = mean(red_zstack)
+sig_red = std(red_zstack)*2
+
+hm2a = heatmap!(ax1a, xlims, ylims, grn_zstack[:,:,1], colormap = Reverse(:algae), colorrange = (0.0, mu_grn + sig_grn))
+hm2b = heatmap!(ax1b, xlims, ylims, red_zstack[:,:,1], colormap = :gist_heat, colorrange = (0.0, mu_red + 2sig_red), alpha = 1.0)
+
+lines!(ax2a, data2P.t, grn_trace, color = :green)
+lines!(ax2b, data2P.t, red_trace, color = :red)
+
+ticker1 = vlines!(ax2a, [0.0], color = :black)
+ticker2 = vlines!(ax2b, [0.0], color = :black)
+
+fps = (1/data2P.dt) #Increase speed 5x
+
+GLMakie.record(fig, "animation.mp4", enumerate(data2P.t), framerate = 5fps) do (i, t) 
+    println(t)
+
+    hm2a[3] = grn_zstack[:,:,i]
+    hm2b[3] = red_zstack[:,:,i]
+    ticker1[1] = [t]
+    ticker2[1] = [t] 
+
+end
 fig
