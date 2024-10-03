3b120e3d602e07c9dbf8fa6a59f737cc7f5c8b14
mattar13 <mtarchick13@gmail.com>
2024-10-03 15:35:19 -0700

    moving image utilities out of image utilities and into modify experiments

--- src/Experiment/ModifyExperiments.jl
+++ src/Experiment/ModifyExperiments.jl
@@ -443,17 +443,31 @@ function baseline_adjust!(trace::Experiment{WHOLE_CELL,T};
     end
 end
 
-#I couldn't figure out where to put these functions, 
-#   But I don't think they need to go elsewhere
-function convert_stimulus!(exp::Experiment, channel::String; kwargs...)
-    stimulus_protocol = extractStimulus(exp.HeaderDict; stimulus_name = channel, kwargs...)
-    #println(stimulus_protocol)
-end
-convert_stimulus!(exp::Experiment, n_channel::Int64) = convert_stimulus!(exp, exp.chNames[n_channel])
-
-
-
 #These particular methods are for images only
+function deinterleave!(exp::Experiment{TWO_PHOTON, T}; 
+    n_channels = 2,
+    new_ch_name = "Alexa 594", new_ch_unit = "px"
+) where T<:Real 
+    channel_arr = []
+    for i in 1:n_channels
+        new_ch = exp.data_array[:, i:n_channels:end, :]
+        push!(channel_arr, new_ch)
+    end
+    new_time = exp.t[1:size(channel_arr[1], 2)]
+    new_data_arr = cat(channel_arr..., dims = 3)
+    exp.t = new_time
+    exp.data_array = new_data_arr
+
+    push!(exp.chNames, new_ch_name)
+    push!(exp.chUnits, new_ch_unit)
+    #println(size(exp.data_array))
+    return exp
+end
+
+function project(exp::Experiment{TWO_PHOTON, T}; dims = 3) where T<:Real
+    img_arr = get_all_frames(exp)
+    project_arr = mean(img_arr, dims = dims)
+end
 
 function adjustBC!(exp::Experiment{TWO_PHOTON}; channel = nothing,
     min_val_y = 0.0, max_val_y = 1.0, std_level = 1,
