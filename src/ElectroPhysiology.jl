756b7f731cf632aa6e5087bd7c628e52e1efe18a
mtarchick13@gmail.com <mtarchick13@gmail.com>
2025-05-28 14:16:41 -0700

    Remove export for `convert_channel_to_stimulus!` from ElectroPhysiology.jl as it has been renamed to `addStimulus!`, ensuring clarity and consistency in function naming.

--- src/ElectroPhysiology.jl
+++ src/ElectroPhysiology.jl
@@ -30,7 +30,6 @@ export std,mean
 export addStimulus! #This allows us to either add a stimulus from a StimulusProtocol object or from a file
 export getStimulusStartTime, getStimulusEndTime
 export getStimulusStartIndex, getStimulusEndIndex
-export convert_channel_to_stimulus!
 
 include("Experiment/IterateExperiments.jl")
 export getdata, getchannel, eachchannel, eachtrial
