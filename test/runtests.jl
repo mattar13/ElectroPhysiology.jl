10800bcb75855dbae74f3871613905b61e0fd71d
mattar13 <mtarchick13@gmail.com>
2025-06-19 14:49:21 -0700

    Remove commented-out `addStimulus!` and `addStimulus` functions from `StimulusProtocol.jl` and delete unused ABF test files. Clean up `runtests.jl` by removing references to these tests, streamlining the test setup.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,2 @@
 using Test
 using ElectroPhysiology
-import ElectroPhysiology: Experiment, StimulusProtocol
-
-test_many_traces = "24622008.abf"
-test_single_trace = "24622010.abf"
-
-#We need to open this here to do most of the testing
-include("testExperiments.jl")
-
-include("testABFReader.jl")
-
-#Not ready for this test yet I think
-#include("testStimulusProtocols.jl")
-
-include("testImages.jl")
-
-include("testFilters.jl")
-
-#Test analysis
-using PhysiologyAnalysis
-#Lets write some tests for physiology analysis
-
-
-#Test plotting
-using GLMakie, PhysiologyPlotting
